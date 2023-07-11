775d8c46fa2f78d22798fc95b5942a3ad898c9eb
Angel <angelgonzc@yahoo.com>
2023-07-11 16:51:20 +0100

    web scraping almost done

--- python_db_feeder/teams_screaper.py
+++ python_db_feeder/teams_screaper.py
@@ -10,10 +10,10 @@ def team_scrapper (url):
     html_soup = BeautifulSoup(html.content,"html.parser")
     ### EXTRACT LOGOS ###
     number_of_teams = len(html_soup.findAll("img"))
-    team_logos = []
+    team_badges = []
     for i in range(number_of_teams):
-        src = html_soup.select("img")[0].attrs["src"]
-        team_logos.append(src)
+        src = html_soup.select("img")[i].attrs["src"]
+        team_badges.append(src)
 
     ### EXTRACT NAME AND LINK ###
 
@@ -46,19 +46,21 @@ def team_scrapper (url):
 
     all_teams=[]
     for i in range(len(names_list)):
-        a = Team(names_list[i], all_data_titles[0][i], all_data_titles[1][i], all_data_titles[2][i],
-                  all_data_titles[3][i])
+        a = Team(names_list[i], links_list[i], all_data_titles[0][i], all_data_titles[1][i], all_data_titles[2][i],
+                  all_data_titles[3][i], team_badges[i])
         all_teams.append(a)
     return all_teams, links_list
 
 efl_teams, efl_links= team_scrapper("https://www.fifaindex.com/teams/?league=14&order=desc")
 premier_teams, premier_links= team_scrapper("https://www.fifaindex.com/teams/?league=13&order=desc")
 
-print(premier_teams[1].name)
+# print(premier_teams[9].name)
+# print(premier_teams[8].badge)
 
-print(efl_teams[1].name)
+# print(efl_teams[1].name)
 
-for i in range(len(premier_teams)):
-    print(premier_teams[i].name)
+# for i in range(len(premier_teams)):
+#     print(premier_teams[i].name)
 
-print(efl_links)
+# print(efl_links)
+
