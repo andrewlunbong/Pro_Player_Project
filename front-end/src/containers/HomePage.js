2e04599cb5fc5fb6079221961023d97751b047c3
andrewlunbong <andrewlunbong@gmail.com>
2023-07-11 16:41:24 +0100

    working on it

---
+++ front-end/src/containers/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+     <NavBar/>
+    </div>
+  );
+};
+
+export default HomePage;
