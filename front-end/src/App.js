a4f7bf151260d763db9e79fbde22d63cf5dd83ce
andrewlunbong <andrewlunbong@gmail.com>
2023-07-14 11:22:03 +0100

    working on frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,8 +14,8 @@ function App() {
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/squad" element={<SquadPage />} />
         <Route path="/player" element={<PlayerSeasonStats />} />
-        <Route path='/decision' element = {<Decision/>}/>
-        <Route path='/timer' element= {<Timer/>}/>
+        {/* <Route path='/decision' element = {<Decision/>}/>
+        <Route path='/timer' element= {<Timer/>}/> */}
         
       </Routes>
     </Router>
